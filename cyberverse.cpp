cdf595869da180bb21d646c6abc624cc6b9dc39c
ishitaoverhere <ishita1395.cse19@chitkara.edu.in>
2021-09-08 22:11:23 +0530

    2

---
+++ cyberverse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, k, i;
+        //n-number of kids
+        //k-number of weapons
+        cin >> n >> k;
+        cout << k/n << endl;
+    }
+}
