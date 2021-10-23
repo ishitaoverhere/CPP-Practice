3124c658fdc5bd98a28719ff7fbea6b261b1a487
ishitaoverhere <ishita1395.cse19@chitkara.edu.in>
2021-10-23 15:12:37 +0530

    push

--- bin_flip.cpp
+++ bin_flip.cpp
@@ -1,33 +1,55 @@
- #include <iostream> 
-
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define FAST1 ios_base::sync_with_stdio(false);
+#define FAST2 cin.tie(NULL);
+#define allsort(a) sort(a.begin(),a.end())
+
+ll n,k;
+void solve(){
+    cin>>n>>k;
+    if(k==0){
+        cout<<"Yes"<<endl;
+        cout<<0<<endl;
+        return;
+    }
+    if(k%2==0){
+        cout<<"No"<<endl;;
+        return;
+    }
+    ll sz=0;
+    for(ll i=31;i>=0;i--){
+        if(((1<<i)&k)!=0){
+            sz=i+1;
+            break;
+        }
+    }
+    k=(k+(1<<sz)-1)/2;
+    cout<<"Yes"<<endl;
+    cout<<sz<<endl;
+    int ans=1;
+    vector<int> a;
+    for(int i=sz-2;i>=0;i--){
+        if(((1<<i)&k)!=0){
+            a.push_back(ans);
+            ans+=(1<<i);
+        }
+        else{
+            ans-=(1<<i);
+            a.push_back(ans);
+        }
+    }
+    for(int i=sz-2;i>=0;i--)
+        cout<<a[i]<<endl;
+    cout<<ans<<endl;
+}
 
 int main(){
-	// your code goes her
-	int t;
-	cin>>t;
-	while(t--){
-	 int n,k,j;
-	 double i;
-	 cin>>n>>k;
-	 if(k==0){
-	  cout<<"YES"<<"\n"<<0<<endl;
-	 }
-	 else if (k%2!=0){
-	  i=log2(k+1);
-	  j=(int)i;
-	  if((i-j)==0){
-	   cout<<"YES"<<endl;
-	   for(j=0;j<i;j++){
-	    cout<<pow(2,j)<<endl;
-	   }
-	  }
-	 
-	  else{
-	   cout<<"NO"<<endl;
-	  }
-	 }
-	} 
-	return 0;
+    FAST1;
+    FAST2;
+    ll t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
 }
