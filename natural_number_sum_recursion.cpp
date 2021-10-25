89b2eca54540213f7929c45fa7d51181fa54dcd5
ishitaoverhere <ishita1395.cse19@chitkara.edu.in>
2021-10-25 13:03:04 +0530

    push

---
+++ natural_number_sum_recursion.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int sum(int n) {
+    if(n != 0)
+        return n + sum(n - 1);
+    return 0;
+}
+
+int main() {
+    int n;
+
+    cout << "Enter a natural number: ";
+    cin >> n;
+
+    cout << sum(n);
+
+    return 0;
+}
