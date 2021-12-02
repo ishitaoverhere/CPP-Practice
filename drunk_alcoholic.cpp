7352403049900a784ef0b66556a3bba728675f95
ishitaoverhere <ishita1395.cse19@chitkara.edu.in>
2021-12-02 11:24:53 +0530

    commit

---
+++ drunk_alcoholic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int k;
+        cin>>k;
+        cout<<k+2<<"\n";
+    }
+    return 0;
+}
